15568ef59357e3c84b15b1dc05cfbeb6ac2650ae
gryf <gryf73@gmail.com>
2016-11-18 19:07:17 +0100

    Added undocumented fields for User object

--- slack_backup/objects.py
+++ slack_backup/objects.py
@@ -62,18 +62,22 @@ class UserProfile(object):
 
         data_dict = data_dict or {}
 
-        self.first_name = data_dict.get("first_name", "")
-        self.last_name = data_dict.get("last_name", "")
-        self.real_name = data_dict.get("real_name", "")
+        self.avatar_hash = data_dict.get("avatar_hash", "")
         self.email = data_dict.get("email", "")
-        self.skype = data_dict.get("skype", "")
-        self.phone = data_dict.get("phone", "")
+        self.first_name = data_dict.get("first_name", "")
+        self.image_1024 = data_dict.get("image_1024", "")
+        self.image_192 = data_dict.get("image_192", "")
         self.image_24 = data_dict.get("image_24", "")
         self.image_32 = data_dict.get("image_32", "")
         self.image_48 = data_dict.get("image_48", "")
-        self.image_72 = data_dict.get("image_72", "")
-        self.image_192 = data_dict.get("image_192", "")
         self.image_512 = data_dict.get("image_512", "")
+        self.image_72 = data_dict.get("image_72", "")
+        self.image_original = data_dict.get("image_original", "")
+        self.last_name = data_dict.get("last_name", "")
+        self.phone = data_dict.get("phone", "")
+        self.real_name = data_dict.get("real_name", "")
+        self.real_name_normalized = data_dict.get("real_name_normalized", "")
+        self.skype = data_dict.get("skype", "")
 
 
 class User(BaseObject):
@@ -81,16 +85,23 @@ class User(BaseObject):
         data_dict = data_dict or {}
 
         self._id = data_dict.get("id", "")
-        self.name = data_dict.get("name", "")
-        self.deleted = data_dict.get("deleted", False)
         self.color = data_dict.get("color", "")
+        self.deleted = data_dict.get("deleted", False)
+        self.has_2fa = data_dict.get("has_2fa", False)
+        self.has_files = data_dict.get("has_files", False)
         self.is_admin = data_dict.get("is_admin", False)
+        self.is_bot = data_dict.get("tz", "")
         self.is_owner = data_dict.get("is_owner", False)
         self.is_primary_owner = data_dict.get("is_primary_owner", False)
         self.is_restricted = data_dict.get("is_restricted", False)
         self.is_ultra_restricted = data_dict.get("is_ultra_restricted", False)
-        self.has_2fa = data_dict.get("has_2fa", False)
+        self.name = data_dict.get("name", "")
+        self.real_name = data_dict.get("real_name", "")
+        self.status = data_dict.get("status", "")
+        self.team_id = data_dict.get("team_id", "")
         self.two_factor_type = data_dict.get("two_factor_type", "")
-        self.has_files = data_dict.get("has_files", False)
+        self.tz = data_dict.get("tz", "")
+        self.tz_label = data_dict.get("tz_label", "")
+        self.tz_offset = data_dict.get("tz_offset", "")
 
         self.profile = UserProfile(data_dict.get("profile"))
